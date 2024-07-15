cad81279686264f64c84653fde0dc4e474750c91
Chirag <chiragsharma2002123@gmail.com>
2024-07-15 06:09:59 +0530

    Building my app

---
+++ Building our Application/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * Header
+ * - Logo
+ * - Nav Items
+ *
+ * Body
+ * - Search
+ * - Restaurent Container
+ *   - Restrauant Card
+ *
+ * Footer
+ * - Copyright
+ * - Links
+ * - Address
+ * - Contact
+ *
+ */
