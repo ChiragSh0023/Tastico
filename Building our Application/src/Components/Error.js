8e99f3240667557cb6bc29c909cdefa18c2d8f4b
Chirag <chiragsharma2002123@gmail.com>
2024-11-10 13:54:55 +0530

    Styling Error Component

--- Building our Application/src/Components/Error.js
+++ Building our Application/src/Components/Error.js
@@ -1,12 +1,12 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import pageNotFound from "../assets/img/pageNotFound.jpg";
 
 const Error = () => {
   const err = useRouteError();
   console.log(err);
   return (
-    <>
-      <img src={pageNotFound} className="err-img" />
+    <div className="error-container">
+      <img src={pageNotFound} className="err-img" height="257" width="257" />
       <h1 className="error-ele">Oops! Something went wrong.</h1>
       <h2 className="error-ele">
         The page you're looking for doesn't exist or an error occurred.
@@ -14,7 +14,12 @@ const Error = () => {
       <p className="error-ele">
         {err.status} : {err.statusText}
       </p>
-    </>
+      <button className="item-add-btn error-ele">
+        <Link to="/" style={{ color: "black" }}>
+          Go to Home!
+        </Link>
+      </button>
+    </div>
   );
 };
 
