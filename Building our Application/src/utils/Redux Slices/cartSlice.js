dd5fb831b02c263803f89cdffdfd478c38f1cfdc
ChiragSh0023 <chiragsharma2002123@gmail.com>
2024-12-12 19:54:20 +0530

    Updated Logo for the app

--- Building our Application/src/utils/Redux Slices/cartSlice.js
+++ Building our Application/src/utils/Redux Slices/cartSlice.js
@@ -25,7 +25,7 @@ const cartSlice = createSlice({
       } else {
         state.items.push({ ...action.payload, quantity: 1 });
       }
-      console.log(current(state.items));
+      // console.log(current(state.items));
     },
     removeItem: (state, action) => {
       const itemIndex = state.items.findIndex(
